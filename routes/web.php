e4b57c805a9187802ab5d15b1f1118b1f60778ac
Alvin A. Rahman <alviinaditya@gmail.com>
2023-03-14 00:37:55 +0700

    Add Status Model, Factory and Migration

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::get('/timeline', TimelineController::class)->name('timeline');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    Route::get('/timeline', TimelineController::class)->name('timeline'); 
+});
 
 require __DIR__.'/auth.php';
