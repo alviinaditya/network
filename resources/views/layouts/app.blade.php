44a5c1db82252a0679f45d38e4ca92926c90edba
alviinaditya <alviinaditya@gmail.com>
2023-03-17 13:35:48 +0700

    Add dark mode dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,12 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
             @isset($header)
                 <!-- Page Heading -->
-                <header class="bg-white border-b">
+                <header class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
